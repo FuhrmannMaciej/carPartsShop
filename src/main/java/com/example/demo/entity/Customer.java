7308c504d2b4fea0ae37e47f19b2ad2feee11d07
fuhrmann.maciej <fuhrmann.maciej@gmail.com>
2022-05-22 11:12:45 +0200

    Created Customer entity

---
+++ src/main/java/com/example/demo/entity/Customer.java
@@ -0,0 +1,26 @@
+package com.example.demo.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+}
