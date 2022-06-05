883d00f960b0b17f40d032e4546fdee0008dbac6
fuhrmann.maciej <fuhrmann.maciej@gmail.com>
2022-06-05 12:57:55 +0200

    Added CarPart and CarModel entities, created relationship between them.

---
+++ src/main/java/com/example/demo/entity/CarModel.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "car_model")
+public class CarModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "yearOfProduction")
+    private int yearOfProduction;
+
+    @ManyToMany(mappedBy = "carModels")
+    private Collection<CarPart> carParts;
+
+}
---
+++ src/main/java/com/example/demo/entity/CarPart.java
@@ -0,0 +1,34 @@
+package com.example.demo.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "car_part")
+public class CarPart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "car_part_car_model", joinColumns = @JoinColumn(name = "car_part_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "car_model_id", referencedColumnName = "id"))
+    private Collection<CarModel> carModels;
+}
