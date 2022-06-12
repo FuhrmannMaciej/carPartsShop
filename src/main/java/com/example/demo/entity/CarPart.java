7c3122c74e41dc9d9d3a1bb0168888684c1146d0
fuhrmann.maciej <fuhrmann.maciej@gmail.com>
2022-06-12 19:47:37 +0200

    Updated id annotation from Auto to Identity, code refactored.

--- src/main/java/com/example/demo/entity/CarPart.java
+++ src/main/java/com/example/demo/entity/CarPart.java
@@ -16,7 +16,7 @@ import java.util.Collection;
 public class CarPart {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
