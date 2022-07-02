e8b730046e6ac4a40e5cc8ea9ac64121fc726adc
fuhrmann.maciej <fuhrmann.maciej@gmail.com>
2022-07-02 18:56:31 +0200

    Implemented sent method in main application class, enabled scheduling for sent method.

--- carPartsShopManagement/src/main/java/com/carPartsShop/CarPartsShopApplication.java
+++ carPartsShopManagement/src/main/java/com/carPartsShop/CarPartsShopApplication.java
@@ -1,13 +1,27 @@
 package com.carPartsShop;
 
+import com.carPartsShop.service.CarPartModelProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class CarPartsShopApplication {
+
+	@Autowired
+	private CarPartModelProducerService service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarPartsShopApplication.class, args);
+	}
+
+	@Scheduled(fixedRate = 5000)
+	public void postCarPartModel() {
+		service.send();
+		System.out.println("Posting car part models");
 	}
 
 }
