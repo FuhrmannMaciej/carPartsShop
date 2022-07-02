736e9a46b3ca20a01ea5b8c72998aa03abca3b9d
fuhrmann.maciej <fuhrmann.maciej@gmail.com>
2022-07-02 20:39:34 +0200

    Implement sync and carPartModelUpdate methods and add send mechanism for both

--- carPartsShopManagement/src/main/java/com/carPartsShop/service/CarPartModelProducerService.java
+++ carPartsShopManagement/src/main/java/com/carPartsShop/service/CarPartModelProducerService.java
@@ -4,9 +4,9 @@ import com.carPartsShop.config.AppConstants;
 import com.carPartsShop.entity.CarModel;
 import com.carPartsShop.entity.CarPart;
 import com.carPartsShop.repository.CarModelRepository;
+import com.carPartsShop.kafka.dto.CarPartModelKafkaDto;
 import com.carPartsShop.repository.CarPartRepository;
-import com.carPartsShop.web.dto.CarPartModelKafkaDto;
-import org.hibernate.Hibernate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -14,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @Transactional
 public class CarPartModelProducerService {
@@ -22,26 +23,44 @@ public class CarPartModelProducerService {
     private KafkaTemplate<String, CarPartModelKafkaDto> kafkaTemplate;
 
     @Autowired
-    private CarModelRepository carModelRepository;
-
-    @Autowired
     private CarPartRepository carPartRepository;
 
-    public void send() {
-        CarPartModelKafkaDto json = new CarPartModelKafkaDto();
+    @Autowired
+    private CarModelRepository carModelRepository;
+
+    public void syncCarPartModels() {
+        int counter = 0;
         List<CarModel> carModels = carModelRepository.findAll();
 
         for (CarModel carModel : carModels) {
             List<CarPart> carParts = carModel.getCarParts().stream().toList();
             for (CarPart carPart : carParts) {
-                json.setCarModelName(carModel.getName());
-                json.setYearOfProduction(carModel.getYearOfProduction());
-                json.setCarPartName(carPart.getName());
-                json.setPrice(carPart.getPrice());
-                json.setQuantity(carPart.getQuantity());
-
+                CarPartModelKafkaDto json = createCarPartModelKafkaDto(carModel, carPart);
+                counter++;
                 kafkaTemplate.send(AppConstants.TOPIC, json);
             }
         }
+        log.info("Sent {} car part models to Kafka", counter);
+    }
+
+    private CarPartModelKafkaDto createCarPartModelKafkaDto(CarModel carModel, CarPart carPart) {
+        CarPartModelKafkaDto json = new CarPartModelKafkaDto();
+        json.setCarModelSourceSystemId(carModel.getId());
+        json.setCarPartSourceSystemId(carPart.getId());
+        json.setCarModelName(carModel.getName());
+        json.setYearOfProduction(carModel.getYearOfProduction());
+        json.setCarPartName(carPart.getName());
+        json.setPrice(carPart.getPrice());
+        json.setQuantity(carPart.getQuantity());
+        return json;
+    }
+
+    public void sendCarPartModelUpdate(Long carModelId, Long carPartId) {
+        CarModel carModel = carModelRepository.findById(carModelId).orElseThrow();
+        CarPart carPart = carPartRepository.findById(carPartId).orElseThrow();
+
+        CarPartModelKafkaDto json = createCarPartModelKafkaDto(carModel, carPart);
+
+        kafkaTemplate.send(AppConstants.TOPIC, json);
     }
 }
