1eb9e21d3d658f4b8e8f31247d4f13dcaa185d45
fuhrmann.maciej <fuhrmann.maciej@gmail.com>
2022-07-05 20:04:56 +0200

    Disabled scheduling, fixed docker-compose.yml, small changes

--- carPartsShopManagement/src/main/java/com/carPartsShopManagement/CarPartsShopApplication.java
+++ carPartsShopManagement/src/main/java/com/carPartsShopManagement/CarPartsShopApplication.java
@@ -1,27 +1,13 @@
 package com.carPartsShopManagement;
 
-import com.carPartsShopManagement.service.CarPartModelProducerService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+
 
 @SpringBootApplication
-@EnableScheduling
 public class CarPartsShopApplication {
-
-	@Autowired
-	private CarPartModelProducerService service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarPartsShopApplication.class, args);
 	}
-
-	@Scheduled(fixedRate = 600000)
-	public void postCarPartModel() {
-		service.syncCarPartModels();
-		System.out.println("Posting car part models");
-	}
-
 }
